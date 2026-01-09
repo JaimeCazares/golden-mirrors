9db9518c27b30572637a535634b2e4b0635c970f
Jaime Cazares <jaimecazares136@gmail.com>
2026-01-09 03:53:10 -0700

    cel 1

--- ahorrovaleria/ahorro.php
+++ ahorrovaleria/ahorro.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'novia') {
 <html lang="es">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Mi ahorro 💖</title>
 <link rel="stylesheet" href="ahorro.css">
 </head>
